ecfe1f800725b4043a4c4363a94da983360ae1b8
Santiago Triana <a.trianaa@uniandes.edu.co>
2021-05-10 17:05:20 -0500

    Test req3 terminado

---
+++ st-parranderos-jdo-est/src/test/java/uniandes/isis2304/vacuandes/test/Req3.java
@@ -0,0 +1,14 @@
+package uniandes.isis2304.vacuandes.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Req3 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
