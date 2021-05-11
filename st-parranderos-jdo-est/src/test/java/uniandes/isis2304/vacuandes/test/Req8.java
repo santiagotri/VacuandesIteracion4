b8eab4db61bc5115e50a6ab419b1e6a8b389dc12
Santiago Triana <a.trianaa@uniandes.edu.co>
2021-05-11 17:54:40 -0500

    Auto stash before merge of "main" and "origin/main"

---
+++ st-parranderos-jdo-est/src/test/java/uniandes/isis2304/vacuandes/test/Req8.java
@@ -0,0 +1,14 @@
+package uniandes.isis2304.vacuandes.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Req8 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
